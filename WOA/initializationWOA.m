0e78a172116dc8e297dda3c60b62f2b457eaa5a0
ViniNanaki <106417053+ViniNanaki@users.noreply.github.com>
2023-10-16 16:04:07 -0300

    Add files via upload

---
+++ WOA/initializationWOA.m
@@ -0,0 +1,36 @@
+%_________________________________________________________________________%
+%  Whale Optimization Algorithm (WOA) source codes demo 1.0               %
+%                                                                         %
+%  Developed in MATLAB R2011b(7.13)                                       %
+%                                                                         %
+%  Author and programmer: Seyedali Mirjalili                              %
+%                                                                         %
+%         e-Mail: ali.mirjalili@gmail.com                                 %
+%                 seyedali.mirjalili@griffithuni.edu.au                   %
+%                                                                         %
+%       Homepage: http://www.alimirjalili.com                             %
+%                                                                         %
+%   Main paper: S. Mirjalili, A. Lewis                                    %
+%               The Whale Optimization Algorithm,                         %
+%               Advances in Engineering Software , in press,              %
+%               DOI: http://dx.doi.org/10.1016/j.advengsoft.2016.01.008   %
+%                                                                         %
+%_________________________________________________________________________%
+
+% This function initialize the first population of search agents
+function Positions=initialization(SearchAgents_no,dim,ub,lb)
+
+Boundary_no= size(ub,2); % numnber of boundaries
+
+% If the boundaries of all variables are equal and user enter a signle
+% number for both ub and lb
+if Boundary_no==1
+    Positions=rand(SearchAgents_no,dim).*(ub-lb)+lb;
+end
+
+% If each variable has a different lb and ub
+if Boundary_no>1
+    for i=1:dim
+        Positions(:,i)= randperm(256, SearchAgents_no) - 1;
+    end
+end

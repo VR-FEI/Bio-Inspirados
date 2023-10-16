dda34b10f2e82b1b18ac53892f8a6b7c6b54c6e2
ViniNanaki <106417053+ViniNanaki@users.noreply.github.com>
2023-10-16 16:05:24 -0300

    Add files via upload

---
+++ KH/cost.m
@@ -0,0 +1,16 @@
+function f=cost(X,H, q)
+%funcao custo de Ackley
+%f = ackley(X);
+%f = morefood(X);
+
+%f = krill_shannon(X,H);
+if q == 1
+    f=feval('psrAvaliacaoShannon',H, X');
+else
+    f=feval('psrAvaliacaoTsallis',H, q, X');         
+end
+
+if isnan(f)
+  f = 1000;
+end
+

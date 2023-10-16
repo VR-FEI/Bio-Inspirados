6caa9aaeed0ec4615f8d2f7957fc9a508a3862e4
ViniNanaki <106417053+ViniNanaki@users.noreply.github.com>
2023-10-16 16:20:52 -0300

    Add files via upload

---
+++ GOA/initializationGOA.m
@@ -0,0 +1,32 @@
+%_________________________________________________________________________%
+%  Grasshopper Optimization Algorithm (GOA) source codes demo V1.0        %
+%                                                                         %
+%  Developed in MATLAB R2016a                                             %
+%                                                                         %
+%  Author and programmer: Seyedali Mirjalili                              %
+%                                                                         %
+%         e-Mail: ali.mirjalili@gmail.com                                 %
+%                 seyedali.mirjalili@griffithuni.edu.au                   %
+%                                                                         %
+%       Homepage: http://www.alimirjalili.com                             %
+%                                                                         %
+%  Main paper: S. Saremi, S. Mirjalili, A. Lewis                          %
+%              Grasshopper Optimisation Algorithm: Theory and Application %
+%               Advances in Engineering Software , in press,              %
+%               DOI: http://dx.doi.org/10.1016/j.advengsoft.2017.01.004   %
+%                                                                         %
+%_________________________________________________________________________%
+%
+%This function randomly initializes the position of agents in the search space.
+function [X]=initialization(N,dim,up,down)
+
+if size(up,1)==1
+    X=rand(N,dim).*(up-down)+down;
+ 
+end
+if size(up,1)>1 
+    for i=1:dim
+        X(:,i)= randperm(256, N) - 1;
+    end
+end
+X = sort(X')';
